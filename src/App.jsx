8f1f3828bc8349d26b2d56c2344de018a76548fb
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-01-20 20:41:37 +0200

    create PageNav

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
 import PageNotFound from './pages/PageNotFound';
+import PageNav from './components/PageNav';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="product" element={<Product />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <PageNav />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
