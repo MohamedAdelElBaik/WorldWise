7a92003f8d05dbda20a51d68922f08da73637d13
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-01-26 07:07:02 +0200

    CREATE login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import './App.css';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
-import PageNav from './components/PageNav';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <PageNav />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
